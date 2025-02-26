02084bc2f45bd7521537e4258a56bd2d3ea2560a
Tindyy <vdgraaf.tessa@gmail.com>
2025-02-26 21:03:15 +0100

    #8

--- public/script.js
+++ public/script.js
@@ -2,16 +2,19 @@ document.querySelectorAll('.pack').forEach(pack => {
     pack.addEventListener('click', function (event) {
         event.preventDefault(); 
 
-
+        console.log("Pack clicked, hiding team-pack-container...");
         document.querySelector('.team-pack-container').classList.add('hidden');
 
         const loader = document.getElementById('loader');
+        console.log("Showing loader...");
         loader.classList.remove('hidden');
 
         setTimeout(() => {
+            console.log("Hiding loader...");
             loader.classList.add('hidden'); 
-            document.querySelector('.team-container').classList.remove('hidden'); 
-        }, 2500); 
+
+            console.log("Showing team container...");
+            document.querySelector('.team-container').classList.remove('hidden');
+        }, 2000); 
     });
 });
-
